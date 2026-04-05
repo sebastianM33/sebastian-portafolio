d35f44cb88c286ac00d0886eb2b0487b647cce3d
sebastianM33 <sebasmunoz153@gmail.com>
2026-04-05 16:13:41 -0500

    fix: ignore vite version type mismatch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop'
+    }
+  },
 
   vite: {
-    plugins: [tailwindcss()],
+    // @ts-ignore --- Esto ignorará el error visual en VS Code
+    plugins: [tailwindcss()], 
     ssr: {
       noExternal: []
     }
